2f307380037714cbb9405f755926e7a89c4b1523
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-01-05 20:01:45 -0300

    Partial Views

---
+++ BlogMVC/Models/ViewModels/PostListViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace BlogMVC.Models.ViewModels {
+    public class PostListViewModel {
+        public IEnumerable<Post> Posts { get; set; }
+        public string? Email { get; set; }
+    }
+}
